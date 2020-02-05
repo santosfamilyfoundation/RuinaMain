2b98354583b5dc8788c69821f3f9c4a840224c83
Danny Kang <kdy304g@gmail.com>
2020-02-05 16:00:02 -0500

    change app name

--- android/app/src/main/java/com/ruina/MainApplication.java
+++ android/app/src/main/java/com/ruina/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.ruina;
 
 import android.app.Application;
 import android.content.Context;
--- ios/Pods/Target Support Files/Pods-MyApp-tvOSTests/Pods-MyApp-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyApp_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_MyApp_tvOSTests
-@end
