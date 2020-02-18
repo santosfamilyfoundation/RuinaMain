97d470ccdb6b3fb53d7a20898596e01f879a601f
Allison <allisonlynnbasore14@gmail.com>
2020-02-18 11:25:58 -0500

    saved old diagram view

--- screens/diagramViewScreen.js
+++ screens/diagramViewScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView } from 'react-navigation';
-import DiagramView from '../components/DiagramView'
+import DiagramView from '../components/DiagramView2'
 
 export const DiagramViewScreen = ({ navigation }) => {
   return (
