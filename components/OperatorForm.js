3a4fdf1f3ab079f94aac28cd52d4f2238757cb62
Danny Kang <kdy304g@gmail.com>
2020-03-10 05:28:30 -0400

    added question to operator form

--- components/OperatorForm.js
+++ components/OperatorForm.js
@@ -34,6 +34,12 @@ class OperatorForm extends Component{
                         style={{borderWidth:5, borderColor: 'black', height: 40}}
                         defaultValue= {operator.response && operator.response['C1-S2'] ? `  ${operator.response['C1-S2']}` : ''}
                     />
+                    <Text>C1-S3</Text>
+                    <TextInput
+                        onChangeText={content => updateOperator('C1-S3', id, content)}
+                        style={{borderWidth:5, borderColor: 'black', height: 40}}
+                        defaultValue= {operator.response && operator.response['C1-S3'] ? `  ${operator.response['C1-S3']}` : ''}
+                    />
                 </Card>
 
         )
