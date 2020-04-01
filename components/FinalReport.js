f2715c423ecbac9d5b02d563e3406cc905a58995
Danny Kang <kdy304g@gmail.com>
2020-04-01 15:17:31 +0900

    displaying json data on final report

--- components/FinalReport.js
+++ components/FinalReport.js
@@ -2,16 +2,48 @@ import React, {Component} from 'react';
 import { SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
 import { Text } from 'react-native';
-import {TopNavigation} from '@ui-kitten/components';
+import {TopNavigation,Card, CardHeader} from '@ui-kitten/components';
 
 
 class FinalReport extends Component {
     render(){
 
+        const {
+            navigation,
+            driver,
+            nonmotorist,
+            vehicle,
+            passenger,
+            } = this.props
+        console.log(JSON.stringify(vehicle.data[0]));
+        const VehicleHeader = () => (
+            <CardHeader title="Vehicle"/>
+        )
+        const PassengerHeader = () => (
+            <CardHeader title="Passenger"/>
+        )
+        const NonMotoristHeader = () => (
+            <CardHeader title="Non Motorist"/>
+        )
+        const DriverHeader = () => (
+            <CardHeader title="Driver"/>
+        )
         return(
             <SafeAreaView style={{flex:1}}>
                 <TopNavigation title="Final Report" alignment="center" leftControl={this.props.BackAction()}/>
-                    <Text>Final Report Screen</Text>
+                <Card header={VehicleHeader}>
+                    <Text>{JSON.stringify(vehicle.data)}</Text>
+                </Card>
+                <Card header={DriverHeader}>
+                    <Text>{JSON.stringify(driver.data)}</Text>
+                </Card>
+                <Card header={PassengerHeader}>
+                    <Text>{JSON.stringify(passenger.data)}</Text>
+                </Card>
+                <Card header={NonMotoristHeader}>
+                    <Text>{JSON.stringify(nonmotorist.data)}</Text>
+                </Card>
+
             </SafeAreaView>
         )
     }
