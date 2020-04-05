60685a8adfb345752542f6c2557bdf1669f2edeb
Allison Basore <allison.basore@students.olin.edu>
2020-04-04 22:04:04 -0400

    added rest of gallery files

---
+++ components/PhotoGallery.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { TextInput, StyleSheet, View, ScrollView, Image } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { connect } from 'react-redux';
+import { Button, TopNavigation, Text, Card, CardHeader, Layout, Icon } from '@ui-kitten/components';
+import { styles } from './PhotoGallery.style';
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
+class PhotoGallery extends Component {
+
+    render(){
+        const { photos, navigation } = this.props;
+
+        const navigatePhotos = () => {
+            navigation.navigate('PhotoCapture')
+        }
+
+        const allPhotos = [];
+        let index = 0;
+
+        for (let [tag, imagesArray] of Object.entries(photos.images)) {
+          imagesArray.forEach(elm => {
+            index+=1
+            console.log(elm)
+            allPhotos.push(
+              <Card key={index} style={styles.itemCard}>
+                  <View style={styles.itemCardContent}>
+                      <Image source={{uri:elm}} style={styles.image}/>
+                      <Text style={styles.itemCardFooter} category="s1">{tag}</Text>
+                  </View>
+              </Card>
+            )
+          })
+        }
+        const PhotosHeader = () => (
+            <CardHeader title="Photo Gallery" />
+        );
+
+
+        return(
+            <SafeAreaView style={{flex:1}}>
+                <TopNavigation title='Home' style = {{marginBottom: 15}}alignment='center' leftControl={this.props.BackAction()}/>
+                <ScrollView>
+                <Card header={PhotosHeader} style={styles.sectionCard}>
+                    <Layout style={styles.questionContainer}>
+                        {allPhotos}
+                        <Card style={styles.itemCard} onPress = {() => navigatePhotos()}>
+                            <View style={styles.itemCardContent}>
+                                <Icon name='camera-outline' width={75} height={75} />
+                                <Text style={styles.itemCardFooter} category="s1">Take Photo</Text>
+                            </View>
+                        </Card>
+                    </Layout>
+                </Card>
+                </ScrollView>
+            </SafeAreaView>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        photos: state.photosReducer,
+    }
+}
+
+
+export default connect(mapStateToProps)(PhotoGallery);
---
+++ components/PhotoGallery.style.js
@@ -0,0 +1,48 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    sectionCard: {
+        margin: 15,
+    },
+    image :{
+      width: 75,
+      height: 75,
+    },
+    questionContainer: {
+        marginRight: 15,
+        marginBottom: 15,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+    },
+    questionInput: {
+        backgroundColor: 'lightgrey',
+        height: 40,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'blue',
+        paddingLeft: 10
+
+    },
+    headingText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        alignSelf: 'center'
+    },
+    itemCard: {
+        height: 200,
+        width: '30%',
+        margin: 5,
+    },
+    itemCardContent: {
+        height:'100%',
+        justifyContent:'space-evenly',
+        textAlign:'center',
+        alignContent:'center',
+        alignItems:'center',
+    },
+    itemCardFooter: {
+        textAlign:'center',
+    }
+})
