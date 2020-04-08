a3aec426ceb00dcf6b699e7ef23cdd3e405e4cbe
Danny Kang <kdy304g@gmail.com>
2020-04-08 15:57:37 +0900

    finished working on infoExchange Result

--- components/InfoExhange.js
+++ components/InfoExhange.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import {TopNavigation,Card, CardHeader} from '@ui-kitten/components';
 
 
@@ -11,30 +11,76 @@ class InfoExchange extends Component {
         const {
             driver,
             nonmotorist,
-            vehicle,
-            passenger,
             } = this.props
-        console.log(JSON.stringify(vehicle.data[0]));
-        const VehicleHeader = () => (
-            <CardHeader title="Vehicle"/>
-        )
-        const PassengerHeader = () => (
-            <CardHeader title="Passenger"/>
-        )
+
         const NonMotoristHeader = () => (
-            <CardHeader title="Non Motorist"/>
+            <CardHeader title="Non Motorists"/>
         )
+
         const DriverHeader = () => (
-            <CardHeader title="Driver"/>
+
+            <CardHeader title="Drivers"/>
         )
+
+        let infoExchangeResultDriver = {};
+        let infoExchangeResultNonmotorist = {};
+        let infoQuestions = ["P1", "P2", "P3"];
+
+        driver.data.map(driver => {
+            let driverData = {};
+
+            if (driver.response !== undefined) {
+                for (let i=0;i<Object.entries(driver.response).length;i++){
+                    if(infoQuestions.includes(Object.entries(driver.response)[i][0])){
+                        driverData[Object.keys(driver.response)[i]] = Object.entries(driver.response)[i][1]
+                    }
+                }
+                infoExchangeResultDriver[driver.id] = driverData;
+            }
+        })
+
+        nonmotorist.data.map(nonmotorist => {
+            let nonmotoristData = {};
+            if (nonmotorist.response !== undefined){
+                for (let i=0;i<Object.entries(nonmotorist.response).length;i++){
+                    if(infoQuestions.includes(Object.entries(nonmotorist.response)[i][0])){
+                        nonmotoristData[Object.keys(nonmotorist.response)[i]] = Object.entries(nonmotorist.response)[i][1]
+                    }
+                }
+                infoExchangeResultNonmotorist[nonmotorist.id] = nonmotoristData
+            }
+        })
+
+        const renderDrivers =[];
+        for(let i=0;i<Object.entries(infoExchangeResultDriver).length;i++){
+            renderDrivers.push(
+                <Card key={Object.entries(infoExchangeResultDriver)[i][0]} header = {() => (<CardHeader title={Object.entries(infoExchangeResultDriver)[i][1].P1}/>)}>
+                    <Text>
+                        {JSON.stringify(Object.entries(infoExchangeResultDriver)[i][1])}
+                    </Text>
+                </Card>
+            )
+        }
+
+        const renderNonmotorists = [];
+        for(let i=0;i<Object.entries(infoExchangeResultNonmotorist).length;i++){
+            renderNonmotorists.push(
+                <Card key={Object.entries(infoExchangeResultNonmotorist)[i][0]} header = {() => (<CardHeader title={Object.entries(infoExchangeResultNonmotorist)[i][1].P1}/>)}>
+                    <Text>
+                        {JSON.stringify(Object.entries(infoExchangeResultNonmotorist)[i][1])}
+                    </Text>
+                </Card>
+            )
+        }
+        
         return(
             <SafeAreaView style={{flex:1}}>
                 <TopNavigation title="InfoExchange Result" alignment="center" leftControl={this.props.BackAction()}/>
                 <Card header={DriverHeader}>
-                    <Text>{JSON.stringify(driver.data)}</Text>
+                    {renderDrivers}
                 </Card>
                 <Card header={NonMotoristHeader}>
-                    <Text>{JSON.stringify(nonmotorist.data)}</Text>
+                    {renderNonmotorists}
                 </Card>
 
             </SafeAreaView>
