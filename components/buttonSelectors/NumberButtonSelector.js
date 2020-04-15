1ad8c87ac8904383f62830b9a0dab6013f228e56
Peter Seger <segerpeter07@gmail.com>
2020-04-15 11:56:36 +0200

    Fixing question form styling

--- components/buttonSelectors/NumberButtonSelector.js
+++ components/buttonSelectors/NumberButtonSelector.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Button, Text, Layout, Card, CardHeader, Input } from '@ui-kitten/components';
 import { genericWriteAction } from '../../actions/GenericAction';
 import {styles} from './NumberButtonSelector.style'
+import { ScrollView } from 'react-native-gesture-handler';
+import { View } from 'react-native';
 
 
 
@@ -26,21 +28,26 @@ const NumberButtonSelector = (props) => {
 
     return (
         <Card header={Header}>
-            <Layout style={{flex: 1, flexDirection: "row"}}>
-                <Button style={styles.buttonSytle} appearance={(selection == '0') ? 'filled':'outline'} onPress={() => submitField('0')}>None</Button>
-                <Button style={styles.buttonSytle} appearance={(selection == '1') ? 'filled':'outline'} onPress={() => submitField('1')}>1</Button>
-                <Button style={styles.buttonSytle} appearance={(selection == '2') ? 'filled':'outline'} onPress={() => submitField('2')}>2</Button>
-                <Button style={styles.buttonSytle} appearance={(selection == '3') ? 'filled':'outline'} onPress={() => submitField('3')}>3</Button>
-                <Button style={styles.buttonSytle} appearance={(selection == '4') ? 'filled':'outline'} onPress={() => submitField('4')}>4</Button>
-                <Button style={styles.buttonSytle} appearance={(selection == '5') ? 'filled':'outline'} onPress={() => submitField('5')}>5</Button>
-                <Input 
-                    style = {styles.questionInput}
-                    placeholder = "other"
-                    onChangeText = {submitField}
-                    value = {(selection > 5) ? selection:''}
-                    status={(selection > 5) ? 'primary':'basic'}
-                />
-            </Layout>
+            <View style={{flex: 1, flexDirection:"row"}}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={true} contentContainerStyle={styles.container}>
+                    <View style={styles.buttonSection}>
+                        <Button style={styles.buttonStyle} appearance={(selection == '0') ? 'filled':'outline'} onPress={() => submitField('0')}>None</Button>
+                        <Button style={styles.buttonStyle} appearance={(selection == '1') ? 'filled':'outline'} onPress={() => submitField('1')}>1</Button>
+                        <Button style={styles.buttonStyle} appearance={(selection == '2') ? 'filled':'outline'} onPress={() => submitField('2')}>2</Button>
+                        <Button style={styles.buttonStyle} appearance={(selection == '3') ? 'filled':'outline'} onPress={() => submitField('3')}>3</Button>
+                        <Button style={styles.buttonStyle} appearance={(selection == '4') ? 'filled':'outline'} onPress={() => submitField('4')}>4</Button>
+                        <Button style={styles.buttonStyle} appearance={(selection == '5') ? 'filled':'outline'} onPress={() => submitField('5')}>5</Button>
+                    </View>
+                    <View style={styles.questionInput}>
+                        <Input 
+                            placeholder = "other"
+                            onChangeText = {submitField}
+                            value = {(selection > 5) ? selection:''}
+                            status={(selection > 5) ? 'primary':'basic'}
+                        />
+                    </View>
+                </ScrollView>
+            </View>
         </Card>
     )
 }
