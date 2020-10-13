39436eccd7e2308c5ce2ce01eb3fdd039bc20052
coverney <cassandra.overney@students.olin.edu>
2020-10-12 19:59:17 -0700

    Added react-native-mail package and got test email working
    I added the react-native-mail package and verified that it works with Android by creating a send email button in FinalReport and then sending a hardcoded test email whenever the button is pressed.

--- components/FinalReport.js
+++ components/FinalReport.js
@@ -3,11 +3,40 @@ import { SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
 import { Linking } from 'react-native';
 import {TopNavigation,Card, CardHeader, Text, Button} from '@ui-kitten/components';
+import Mailer from 'react-native-mail';
 
 
 class FinalReport extends Component {
+    handleEmail() {
+      console.log('Sending an email!');
+      Mailer.mail({
+        subject: 'Testing',
+        recipients: ['coverney@olin.edu'],
+        // ccRecipients: ['supportCC@example.com'],
+        // bccRecipients: ['supportBCC@example.com'],
+        body: '<b>A Bold Body</b>',
+        customChooserTitle: "This is my new title", // Android only (defaults to "Send Mail")
+        isHTML: true,
+        // attachments: [{
+        //   path: '',  // The absolute path of the file from which to read data.
+        //   type: '',   // Mime Type: jpg, png, doc, ppt, html, pdf, csv
+        //   // mimeType - use only if you want to use custom type
+        //   name: '',   // Optional: Custom filename for attachment
+        // }]
+      }, (error, event) => {
+        console.log('errror', error)
+        Alert.alert(
+          error,
+          event,
+          [
+            {text: 'Ok', onPress: () => console.log('OK: Email Error Response')},
+            {text: 'Cancel', onPress: () => console.log('CANCEL: Email Error Response')}
+          ],
+          { cancelable: true }
+        )
+      });
+    }
     render(){
-
         const {
             navigation,
             driver,
@@ -30,6 +59,9 @@ class FinalReport extends Component {
         const FeedbackHeader = () => (
             <CardHeader title="Feedback"/>
         )
+        const EmailHeader = () => (
+            <CardHeader title="Email"/>
+        )
         return(
             <SafeAreaView style={{flex:1}}>
                 <TopNavigation title="Final Report" alignment="center" leftControl={this.props.BackAction()}/>
@@ -46,17 +78,20 @@ class FinalReport extends Component {
                     <Text>{JSON.stringify(nonmotorist.data)}</Text>
                 </Card>
                 <Card header={FeedbackHeader} style={{marginTop:20}}>
-                <Text style={{marginBottom: 20}}>Tell us what you liked and what you didn't like so we can make your experience better.</Text>
-                <Button onPress={()=>Linking.openURL('https://forms.gle/ho3cZNyoaFArNNN79')}>Submit Feedback</Button>
+                  <Text style={{marginBottom: 20}}>Tell us what you liked and what you didn't like so we can make your experience better.</Text>
+                  <Button onPress={()=>Linking.openURL('https://forms.gle/ho3cZNyoaFArNNN79')}>Submit Feedback</Button>
                 </Card>
-
+                <Card header={EmailHeader}>
+                  <Text style={{marginBottom: 20}}>Press this button to email the crash report.</Text>
+                  <Button onPress={()=>this.handleEmail()}>Email Report</Button>
+                </Card>
             </SafeAreaView>
         )
     }
 }
 
 const mapStateToProps = (state) => {
-    return{        
+    return{
         driver: state.driverReducer,
         nonmotorist: state.nonmotoristReducer,
         vehicle: state.vehicleReducer,
