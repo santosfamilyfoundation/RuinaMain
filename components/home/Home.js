292fdfb9dcb1d266fe33c2f2bed44da011d4273c
Enmoren <enmo.ren@students.olin.edu>
2021-03-03 12:40:59 +0800

    Add edit state value for home screen, cancel not working properly

--- components/home/Home.js
+++ components/home/Home.js
@@ -7,9 +7,36 @@ import { styles } from './Home.style';
 import {questions} from '../../data/questions';
 import VehicleSection from './homeSections/VehicleSection';
 import NonMotoristSection from './homeSections/NonMotoristSection';
+import { addPassenger, deletePassenger } from '../../actions/PassengerAction';
 
-
+const uuid = require('react-native-uuid');
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this._addPassenger = this._addPassenger.bind(this);
+        this._deletePassenger = this._deletePassenger.bind(this);
+
+        this.state = {
+            original_passengers: [],
+            passengers: [],
+            edit: props.edit || false
+        }
+    }
+    _addPassenger () {
+        let passengerID = uuid.v1();
+        addPassenger({id: passengerID, vehicleID: this.props.vehicle.id});
+        this.setState({
+            passengers: [...this.state.passengers, {id: passengerID, vehicleID: this.props.vehicle.id}]
+        })
+    }
+    _deletePassenger (id) {
+        this.props.deletePassenger({passengerID: id});
+        this.setState({
+            passengers: this.state.passengers.filter(passenger => passenger.id != id)
+        })
+    }
+
     filterQuestionsData = (questionType) => {
         return questions.data.filter(question => question.display.includes(questionType));
     }
@@ -23,7 +50,7 @@ class Home extends Component {
             passenger,
             road
             } = this.props
-
+        const { passengers } = this.state;
         const operatorList = [];
         let roadQuestions = this.filterQuestionsData('road');
 
@@ -31,34 +58,23 @@ class Home extends Component {
             navigation.navigate('Question', {questions: form.data, objectID: id, type})
         }
 
-        const navigatePhotos = () => {
-            navigation.navigate('PhotoCapture')
-        }
-
-        const navigateGallery = () => {
-          navigation.navigate('PhotoGallery')
-        }
-
         const RoadHeader = () => (
-            <CardHeader title="Roadway" />
+            <CardHeader title="Crash and Roadway" />
         );
+//        const {original_passengers} = this.state;
+//        original_passengers = { passengers }
+//        console.log(original_passengers)
 
         const vehiclesListArr = vehicle.data.map((vehicle, index) => {
             operatorList.push({id: vehicle.driver, type:'driver', response:[]})
-
-            if(vehicle?.hazardous) {
+            const { edit } = this.state;
+            if (edit) {
                 return (
                     <VehicleSection
-                        key = {index}
-                        navigation = {navigation}
-                        vehicle = {vehicle}
-                        index = {index}
-                        name = {"(Hazardous) Vehicle"}
-                    />
-                )
-            } else {
-                return (
-                    <VehicleSection
+                        edit = {edit}
+                        passengers = {passengers}
+                        _addPassenger = {this._addPassenger}
+                        _deletePassenger = {this._deletePassenger}
                         key = {index}
                         navigation = {navigation}
                         vehicle = {vehicle}
@@ -66,9 +82,32 @@ class Home extends Component {
                         name = {"Vehicle"}
                     />
                 )
+            } else
+            if (this.props) {
+                    return (
+                        <VehicleSection
+                            passengers = {passengers}
+                            key = {index}
+                            navigation = {navigation}
+                            vehicle = {vehicle}
+                            index = {index}
+                            name = {"Vehicle"}
+                        />
+                    )
+                } else{
+                    return (
+                        <VehicleSection
+                            passengers = {passenger}
+                            key = {index}
+                            navigation = {navigation}
+                            vehicle = {vehicle}
+                            index = {index}
+                            name = {"Vehicle"}
+                        />
+                    )
+                }
             }
-
-        })
+        )
 
         const nonmotoristListArr = nonmotorist.data.map((nonmotorist, index) => {
             operatorList.push({id:nonmotorist.id, type:'nonmotorist', response:[]})
@@ -88,51 +127,90 @@ class Home extends Component {
             <CardHeader title={`Non-motorists`} />
         );
 
-        const infoExchangeIcon = (style) => (
+        const editIcon = (style) => (
             <Icon {...style} name='edit-2-outline' />
         );
 
         const finalReportIcon = (style) => (
             <Icon {...style} name='file-text-outline' />
         );
+        const cancelIcon = (style) => (
+            <Icon {...style} name='log-out' />
+        );
 
-        const rightControls = () => (
-            <View style={styles.rightControlsContainer}>
-                <Layout style={styles.rightControls}>
-                  <TopNavigationAction icon={finalReportIcon} onPress = {() => navigation.navigate('FinalReport')}/>
-                  <Text style={styles.rightControlsText}>Export</Text>
-                </Layout>
-                <Layout style={styles.rightControls}>
-                  <TopNavigationAction icon={infoExchangeIcon} onPress = {() => navigation.navigate('InfoExchangeForm',{ operatorList })}/>
-                  <Text style={styles.rightControlsText}>Info Share</Text>
-                </Layout>
-            </View>
-        )
+        const SaveIcon = (style) => (
+            <Icon {...style} name='save-outline' />
+         );
+
+        const rightControls = () => {
+            const { edit } = this.state
+            if (edit) {
+                return (
+                    <View style={styles.rightControlsContainer}>
+                        <Layout style={styles.rightControls}>
+                            <TopNavigationAction icon={SaveIcon} onPress = {() => {
+                                this.setState({edit: false})
+                            }}/>
+                            <Text style={styles.rightControlsText}>Save</Text>
+                        </Layout>
+                    </View>
+                )
+            } else {
+                return (
+                    <View style={styles.rightControlsContainer}>
+                         <Layout style={styles.rightControls}>
+                           <TopNavigationAction icon={finalReportIcon} onPress = {() =>
+                           {navigation.navigate('FinalReport')}
+                           }/>
+                           <Text style={styles.rightControlsText}>Export</Text>
+                         </Layout>
+                         <Layout style={styles.rightControls}>
+                           <TopNavigationAction icon={editIcon}
+                           onPress = {() =>{
+                                this.setState({edit: true})
+                           }}
+                           />
+                           <Text style={styles.rightControlsText}>Edit</Text>
+                         </Layout>
+                     </View>
+                 )
+            }
+        }
+
+        const leftControls = () => {
+            const { edit } = this.state
+            if (edit) {
+                return (
+                    <View style={styles.rightControlsContainer}>
+                        <Layout style={styles.rightControls}>
+                            <TopNavigationAction icon={cancelIcon} onPress = {() =>
+                            {
+                                this.setState({edit: false});
+                            }
+                            }/>
+                            <Text style={styles.rightControlsText}>Cancel</Text>
+                        </Layout>
+                    </View>
+                )
+            }
+        }
 
         return(
             <SafeAreaView style={{flex:1}}>
-                <TopNavigation title='Home' alignment='center' leftControl={this.props.BackAction()} rightControls = {rightControls()}/>
+                <TopNavigation title='Home' alignment='center' leftControl={leftControls()} rightControls = {rightControls()}/>
                 <ScrollView>
-                    {vehiclesListArr}
-                    <Card key={nonmotorist.id} header = {NonMotoristHeader} style={styles.itemCard} >
-                        <View style={styles.itemCardContent}>
-                            {nonmotoristListArr}
-                        </View>
-                    </Card>
                     <Card header={RoadHeader} style={styles.itemCard}>
                         <View style={styles.itemCardContent}>
                             <Card style={styles.nonMotoristCard} onPress = {() => navigateQuestion(roadQuestions, road.data[0].id, 'Road')}>
                                 <Icon name='paper-plane' width={75} height={75} />
-                                <Text style={styles.itemCardFooter} category="s1">Road</Text>
+                                <Text style={styles.itemCardFooter} category="s1">Crash/Road</Text>
                             </Card>
-                            <Card style={styles.nonMotoristCard} onPress = {() => navigatePhotos()}>
-                                <Icon name='camera-outline' width={75} height={75} />
-                                <Text style={styles.itemCardFooter} category="s1">Take Photo</Text>
-                            </Card>
-                            <Card style={styles.nonMotoristCard} onPress = {() => navigateGallery()}>
-                                <Icon name='archive-outline' width={75} height={75} />
-                                <Text style={styles.itemCardFooter} category="s1">Photos</Text>
-                            </Card>
+                        </View>
+                    </Card>
+                    {vehiclesListArr}
+                    <Card key={nonmotorist.id} header = {NonMotoristHeader} style={styles.itemCard} >
+                        <View style={styles.itemCardContent}>
+                            {nonmotoristListArr}
                         </View>
                     </Card>
                 </ScrollView>
--- components/home/homeSections/VehicleSection.js
+++ components/home/homeSections/VehicleSection.js
@@ -2,51 +2,30 @@ import React, { Component } from 'react';
 import { styles } from '../Home.style';
 import { View} from 'react-native';
 import { Text, Card, CardHeader, Icon } from '@ui-kitten/components';
-import { connect } from 'react-redux';
-import { addPassenger, deletePassenger } from '../../../actions/PassengerAction';
+/**import { connect } from 'react-redux';**/
+/**import { addPassenger, deletePassenger } from '../../../actions/PassengerAction';**/
 import {questions} from '../../../data/questions';
 
-var uuid = require('react-native-uuid');
+/**var uuid = require('react-native-uuid');**/
 
 
 class VehicleSection extends Component{
 
     constructor(props) {
         super(props);
-
-        this._addPassenger = this._addPassenger.bind(this);
-        this._deletePassenger = this._deletePassenger.bind(this);
-
-        this.state = {
-            passengers: []
-        }
-
     }
 
     filterQuestionsData = (questionType) => {
         return questions.data.filter(question => question.display.includes(questionType));
     }
 
-    _addPassenger () {
-        let passengerID = uuid.v1();
-        this.props.addPassenger({id: passengerID, vehicleID: this.props.vehicle.id});
-        this.setState({
-            passengers: [...this.state.passengers, {id: passengerID, vehicleID: this.props.vehicle.id}]
-        })
-    }
-
-    _deletePassenger (id) {
-        this.props.deletePassenger({passengerID: id});
-        this.setState({
-            passengers: this.state.passengers.filter(passenger => passenger.id != id)
-        })
-    }
 
     render(){
-        const {navigation, vehicle, index, name} = this.props
+        const {navigation, vehicle, index, name, passengers, _deletePassenger, _addPassenger, edit} = this.props
         let vehicleQuestions = this.filterQuestionsData('vehicle');
         let passengerQuestions = this.filterQuestionsData('passenger');
         let driverQuestions = this.filterQuestionsData('driver');
+        console.log(edit)
 
         const VehiclesHeader = () => (
             <CardHeader title={`${name} ${index + 1}`} />
@@ -56,47 +35,54 @@ class VehicleSection extends Component{
             navigation.navigate('Question', {questions: form.data, objectID: id, type})
         }
 
-        let passengerListArr = this.state.passengers.map((passenger, index) => {
+        let passengerListArr = passengers.map((passenger, index) => {
             return (
                 <Card key={index} style={styles.individualCard} onPress= {() => navigateQuestion(passengerQuestions, passenger.id, 'Passenger')}
-                onLongPress={() => this._deletePassenger(passenger.id)}>
+                onLongPress={() => _deletePassenger(passenger.id)}>
                     <Icon name='person' width={75} height={75} />
                     <Text style={styles.itemCardFooter} category="s1">Passenger {index+1}</Text>
                 </Card>
             )
         });
 
-        return(
-            <Card key={vehicle.id} header={VehiclesHeader} style={styles.itemCard} >
-                <View style={styles.itemCardContent}>
-                    <Card style={styles.individualCard} onPress= {() => navigateQuestion(vehicleQuestions, vehicle.id, 'Vehicle')}>
-                        <Icon name='car' width={75} height={75} style={{justifyItems:'center', alignItems:'center'}}/>
-                        <Text style={styles.itemCardFooter} category="s1">{name}</Text>
-                    </Card>
-                    <Card style={styles.individualCard} onPress= {() => navigateQuestion(driverQuestions, vehicle.driver, 'Driver')}>
-                        <Icon name='person' width={75} height={75} />
-                        <Text style={styles.itemCardFooter} category="s1">Driver</Text>
-                    </Card>
-                    {passengerListArr}
-                    <Card style={styles.individualCard} onPress= {this._addPassenger} >
-                        <Icon name='plus' width={75} height={75} />
-                        <Text style={styles.itemCardFooter} category="s1">Passenger</Text>
-                    </Card>
-                </View>
-            </Card>
-
-        )
+        if (edit){
+            return(
+                <Card key={vehicle.id} header={VehiclesHeader} style={styles.itemCard} >
+                    <View style={styles.itemCardContent}>
+                        <Card style={styles.individualCard} onPress= {() => navigateQuestion(vehicleQuestions, vehicle.id, 'Vehicle')}>
+                            <Icon name='car' width={75} height={75} style={{justifyItems:'center', alignItems:'center'}}/>
+                            <Text style={styles.itemCardFooter} category="s1">{name}</Text>
+                        </Card>
+                        <Card style={styles.individualCard} onPress= {() => navigateQuestion(driverQuestions, vehicle.driver, 'Driver')}>
+                            <Icon name='person' width={75} height={75} />
+                            <Text style={styles.itemCardFooter} category="s1">Driver</Text>
+                        </Card>
+                        {passengerListArr}
+                        <Card style={styles.individualCard} onPress= {_addPassenger} >
+                            <Icon name='plus' width={75} height={75} />
+                            <Text style={styles.itemCardFooter} category="s1">Passenger</Text>
+                        </Card>
+                    </View>
+                </Card>
+            )
+        } else{
+            return(
+                <Card key={vehicle.id} header={VehiclesHeader} style={styles.itemCard} >
+                    <View style={styles.itemCardContent}>
+                        <Card style={styles.individualCard} onPress= {() => navigateQuestion(vehicleQuestions, vehicle.id, 'Vehicle')}>
+                            <Icon name='car' width={75} height={75} style={{justifyItems:'center', alignItems:'center'}}/>
+                            <Text style={styles.itemCardFooter} category="s1">{name}</Text>
+                        </Card>
+                        <Card style={styles.individualCard} onPress= {() => navigateQuestion(driverQuestions, vehicle.driver, 'Driver')}>
+                            <Icon name='person' width={75} height={75} />
+                            <Text style={styles.itemCardFooter} category="s1">Driver</Text>
+                        </Card>
+                        {passengerListArr}
+                    </View>
+                </Card>
+            )
+        }
 
     }
 }
-
-const mapDispatchToProps = {
-    addPassenger,
-    deletePassenger
-};
-
-const mapStateToProps = (state) => {
-    return state
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(VehicleSection);
+export default VehicleSection
