a226e405f8c8d3fb39ce74fab38910bc1b0c9bb3
Enmoren <31599419+Enmoren@users.noreply.github.com>
2021-03-12 15:38:10 -0500

    Merging dev branch for release (#78)
    * Remove numeric code from multi button selector questions
    
    * Added `OpenTextFieldWithSelection` question type
    
    https://trello.com/c/p79f9bF7
    
    * Update questions.js and constants.js
    
    https://trello.com/c/ApnjRrws
    
    * Updated questions.js
    
    * Restructured quick survey
    
    https://trello.com/c/aMuOnqEs
    
    * Add edit state value for home screen, cancel not working properly
    
    * Completed edit screen part 1 and passenger and non motorist edit functions
    
    * Finished edit home screen feature
    
    Trello cards:
    - https://trello.com/c/fezoxc1e
    - https://trello.com/c/KXA0wkd6
    - https://trello.com/c/x5dpkZHv
    
    * Made minor UI changes to quick survey and home screen
    
    Relevant Trello cards
    - https://trello.com/c/bA06G6tL
    - https://trello.com/c/ZB5oJriP
    - https://trello.com/c/RvHYw5ng
    - https://trello.com/c/eL0hBy5b
    - https://trello.com/c/jmNLl8Ez
    
    Co-authored-by: coverney <cassandra.overney@students.olin.edu>

---
+++ components/buttonSelectors/MultiButtonSelectorQuickSurvey.style.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      marginBottom: 10,
+    },
+    helperText: {
+      marginBottom: 10
+    },
+    content: {
+      margin: 10,
+    },
+    input: {
+      marginBottom: 10,
+      height: 'auto',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    answers: {
+        flex: 1,
+        flexDirection:'row',
+        flexWrap: 'wrap',
+    },
+    answerButton: {
+        marginRight: 10,
+        marginBottom: 10,
+    },
+});
