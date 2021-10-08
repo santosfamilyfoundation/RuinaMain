04ce800478f90dd9a8efb1d105fcd0a74250df3f
Casey May <cmay@olin.edu>
2021-10-08 18:53:37 -0400

    Pushing broken tooltip componentization, close to working

---
+++ components/textFields/Tooltip.style.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    modalContent: {
+      padding: 24,
+      alignItems: 'center',
+    },
+    helperText: {
+      margin: 11
+    }
+});
