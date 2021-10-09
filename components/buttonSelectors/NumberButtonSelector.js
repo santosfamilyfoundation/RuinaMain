460cf611b518eca5c8da42bbcfff33f8e00010dd
Kristin Aoki <kaoki@vgsi.com>
2021-10-09 14:21:43 -0400

    Edit component to be usable outside the context of QuickSurvey

--- components/buttonSelectors/NumberButtonSelector.js
+++ components/buttonSelectors/NumberButtonSelector.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Button, Text, Layout, Card, CardHeader, Input } from '@ui-kitten/components';
-import { genericWriteAction } from '../../actions/GenericAction';
+//import { genericWriteAction } from '../../actions/GenericAction';
 import {styles} from './NumberButtonSelector.style'
 import { ScrollView } from 'react-native-gesture-handler';
 import { View } from 'react-native';
-
+import { updateResponse } from '../../actions/StoryActions';
 
 
 const NumberButtonSelector = (props) => {
     const [visible, setVisible] = React.useState(false);
     const [selection, setSelection] = React.useState('');
-    const {title, data, id, submitFunction, genericReducer, fieldName, updateResponse, dependencyID} = props;
-    // let currId = data.id
+    const {data, id, submitFunction, questionReducer, fieldName, updateResponse, dependencyID} = props;
+
+    let currId = data.id
+    let status = 'danger'
 
     // if(genericReducer[fieldName] != null & selection != genericReducer[fieldName]) {
     //     setSelection(genericReducer[fieldName]);
     // }
+    const reducerData = questionReducer.data.find(entry => entry.id == id);
+    let existingData = !reducerData?.response ? null: reducerData.response;
+
+    // Populate if value already exists in redux
+    if(!selection) {
+        if(existingData != null) {
+            if(existingData[currId] != selection) {
+                setSelection(existingData[currId]);
+            }
+        }
+    }
 
     const submitField = (val) => {
         setSelection(val);
@@ -26,7 +39,19 @@ const NumberButtonSelector = (props) => {
         } else{
             updateResponse && updateResponse({id, question: currId, selection: val})
         }
-        submitFunction(val);
+        submitFunction({id, question: currId, selection: val});
+    }
+
+    if(!existingData) {
+        status = 'danger'
+    } else {
+        if(!existingData[currId]) {
+            status = 'danger';
+        } else if(existingData[currId] != selection) {
+            status = 'danger'
+        } else {
+            status = 'success'
+        }
     }
 
     const ModalContent = () => {
@@ -107,42 +132,53 @@ const NumberButtonSelector = (props) => {
     };
 
     const Header = () => (
-        <CardHeader title={title}/>
+        <CardHeader title={data.question}/>
     );
 
     return (
-        <Card header={Header}>
-            <View style={{flex: 1, flexDirection:"row"}}>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={true} contentContainerStyle={styles.container}>
-                    <View style={styles.buttonSection}>
-                        <Button style={styles.buttonStyle} appearance={(selection == '0') ? 'filled':'outline'} onPress={() => submitField('0')}>None</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '1') ? 'filled':'outline'} onPress={() => submitField('1')}>1</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '2') ? 'filled':'outline'} onPress={() => submitField('2')}>2</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '3') ? 'filled':'outline'} onPress={() => submitField('3')}>3</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '4') ? 'filled':'outline'} onPress={() => submitField('4')}>4</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '5') ? 'filled':'outline'} onPress={() => submitField('5')}>5</Button>
-                    </View>
-                    <View style={styles.questionInput}>
-                        <Input 
-                            placeholder = "other"
-                            onChangeText = {submitField}
-                            value = {(selection > 5) ? selection:''}
-                            status={(selection > 5) ? 'primary':'basic'}
-                        />
-                    </View>
-                </ScrollView>
-            </View>
-        </Card>
+        <Layout style={styles.container}>
+            <Card header={Header} status={status}>
+                <Layout style={styles.content}>
+                        {HelperTooltip()}
+                        <Layout style={styles.input}>
+                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={true} contentContainerStyle={styles.container}>
+
+                            <Layout style={styles.buttonSection}>
+                                <Button style={styles.buttonStyle} appearance={(selection == '0') ? 'filled':'outline'} onPress={() => submitField('0')}>None</Button>
+                                <Button style={styles.buttonStyle} appearance={(selection == '1') ? 'filled':'outline'} onPress={() => submitField('1')}>1</Button>
+                                <Button style={styles.buttonStyle} appearance={(selection == '2') ? 'filled':'outline'} onPress={() => submitField('2')}>2</Button>
+                                <Button style={styles.buttonStyle} appearance={(selection == '3') ? 'filled':'outline'} onPress={() => submitField('3')}>3</Button>
+                                <Button style={styles.buttonStyle} appearance={(selection == '4') ? 'filled':'outline'} onPress={() => submitField('4')}>4</Button>
+                                <Button style={styles.buttonStyle} appearance={(selection == '5') ? 'filled':'outline'} onPress={() => submitField('5')}>5</Button>
+                            </Layout>
+                            <Layout style={styles.questionInput}>
+                                <Input
+                                    placeholder = "other"
+                                    onChangeText = {submitField}
+                                    value = {(selection > 5) ? selection:''}
+                                    status={(selection > 5) ? 'primary':'basic'}
+                                />
+                            </Layout>
+                    </ScrollView>
+                        </Layout>
+                </Layout>
+            </Card>
+        </Layout>
     )
 }
 
 const mapDispatchToProps = {
-    genericWriteAction
+//    genericWriteAction
+    updateResponse
 }
 
 const mapStateToProps = (state, props) => {
-    const genericReducer = state[props.reducerName]
-    return { genericReducer }
+//    const genericReducer = state[props.reducerName]
+//    return { genericReducer }
+    const { response } = state.storyReducer
+    const { reducer } = props;
+    const questionReducer = state[reducer];
+    return { questionReducer, response}
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NumberButtonSelector);
