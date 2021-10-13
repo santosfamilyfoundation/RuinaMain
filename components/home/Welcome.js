acbf5873c9c62df945a40f987fc379f6f6394ab9
18chowdhary <schowdhary@olin.edu>
2021-10-13 11:39:08 -0400

    Create continue report button that enables and disables appropriately

--- components/home/Welcome.js
+++ components/home/Welcome.js
@@ -33,6 +33,7 @@ class Welcome extends Component {
         filePathSelected: false,
         selectedFile: {label: "", value: ""}
       };
+      this.unfinishedReportsPresent = false
     }
 
     async componentDidMount() {
@@ -46,16 +47,19 @@ class Welcome extends Component {
       this.props.resetMap();
       this.props.resetPhoto();
       await this.stateManager.getFilePaths();
-      this.checkAutoSavedSession();
+      this.unfinishedReportsAbsent = this.stateManager.filePaths.length == 0
+      console.log(this.stateManager.filePaths);
+      console.log(this.stateManager.filePaths.length == 0)
+//      this.checkAutoSavedSession();
       this.setState({ loading: false });
     }
 
     checkAutoSavedSession(){
         if (this.stateManager.filePaths != null) {
-            this.setState({autoSavedSession : true, autoSavedSessionDialogBoxVisible : true});
+            this.setState({autoSavedSession : true, filePickerDialogBoxVisible: true});
             console.log('Detect unfinished reports!');
         } else {
-            this.setState({ autoSavedSession: false, autoSavedSessionDialogBoxVisible: false });
+            this.setState({ autoSavedSession: false, filePickerDialogBoxVisible: false });
             console.log('No unfinished report!');
         }
     }
@@ -94,7 +98,13 @@ class Welcome extends Component {
                   <TouchableOpacity style={styles.styledButton} activeOpacity = { .7 } onPress={() => navigateTo('Survey')}>
                       <Text style={styles.btnText}>Start New Report</Text>
                   </TouchableOpacity>
+
+                  <TouchableOpacity style={this.unfinishedReportsAbsent ? styles.styledButtonDisabled : styles.styledButtonEnabled} activeOpacity = { .7 } onPress={() => this.checkAutoSavedSession()}
+                  disabled={this.unfinishedReportsAbsent}>
+                        <Text style={styles.btnText}>Continue Report</Text>
+                  </TouchableOpacity>
                 </View>
+
                 {this.state.displayOutputTest && <Button style={styles.styledButton} onPress={() => navigateTo('OutputPDFTest')}>Test Output PDF</Button>}
               </Layout>
               <Layout style={styles.bottomBar}>
@@ -103,25 +113,6 @@ class Welcome extends Component {
                   {"Built by students at Olin College of Engineering in partnership with the Volpe Center and Santos Family Foundation"}
                 </Text>
               </Layout>
-
-              <MaterialDialog
-                title={"Resume Unfinished Session?"}
-                visible={this.state.autoSavedSessionDialogBoxVisible}
-                okLabel='YES'
-                cancelLabel = 'NO'
-                onCancel={() => {
-                  this.setState({ autoSavedSession: false, autoSavedSessionDialogBoxVisible: false });
-                }}
-                onOk={() => {
-                  this.setState({ autoSavedSession: true,
-                                  autoSavedSessionDialogBoxVisible: false,
-                                  filePickerDialogBoxVisible: true });
-                }}
-              >
-                <Text style={material.subheading}>
-                  Your last session was interrupted unexpectedly. Would you like to resume from where you left off?
-                </Text>
-              </MaterialDialog>
 
               <SinglePickerMaterialDialog
                     title={"Choose the unfinished report you want to continue"}
