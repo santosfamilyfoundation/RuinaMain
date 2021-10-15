9ad31793c41eb1a20b1db74d0ba931c9734a2307
Kristin Aoki <kaoki@vgsi.com>
2021-10-14 21:26:26 -0400

    Add helper function file for repeated functions

---
+++ utils/helperFunctions.js
@@ -0,0 +1,26 @@
+// generate html and convert it into a PDF
+export const createPDF = async (data) => {
+    var converter = new JSONconverter();
+    // const htmlString = converter.handleConverter('pdftest', "");
+    const htmlString = converter.handleConverter('pdf', data);
+    let options = {
+      html: htmlString,
+      base64: true,
+      fileName: 'crash_report',
+    };
+    try {
+      const data = await RNHTMLtoPDF.convert(options);
+      console.log("got PDF data");
+      this.setState({uri: data.filePath, data: data.base64, isPDF:true});
+    } catch (error) {
+      console.log('error->', error);
+    }
+}
+
+// generate default filename
+export const getDefaultFilename = () => {
+    var date = new Date();
+    var localTime = date.toLocaleTimeString().replace(/\W/g, '.');
+    var localDate = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+    return "Crash Report " + localDate + " at " + localTime;
+}
