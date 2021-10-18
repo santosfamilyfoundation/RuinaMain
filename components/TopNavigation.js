9b2d0c99957edd0603687be8c522058184082759
Kristin Aoki <kaoki@vgsi.com>
2021-10-17 21:20:21 -0400

    Add child prop

--- components/TopNavigation.js
+++ components/TopNavigation.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { HStack, Divider, Heading, Icon, IconButton, ArrowBackIcon } from 'native-base';
+import { HStack, Divider, Heading } from 'native-base';
+import Icon from 'react-native-vector-icons'
 
 const TopNavigation = (props) => {
     const { title, backButton, navigation } = props
-
-    console.log(backButton)
 
     return(
         <>
          <HStack>
             {backButton ?
                 <IconButton
-                 icon={<ArrowBackIcon name="arrow-back"/>}
+                 icon={<Icon name="arrow-back" size={25}/>}
                  onPress={()=>{navigation.goBack()}}
                 />
                 : null
             }
             <Heading textAlign="center">{title}</Heading>
+            {props.children}
          </HStack>
          <Divider/>
         </>
