cc3b46704a31235a6c2b6cd55b943eea2b577313
Kristin Aoki <kaoki@vgsi.com>
2021-10-20 15:16:14 -0400

    Update with native base

--- components/buttonSelectors/NumberButtonSelectorQuickSurvey.js
+++ components/buttonSelectors/NumberButtonSelectorQuickSurvey.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Text, Layout, Card, CardHeader, Input } from '@ui-kitten/components';
+import { Button, Text, HStack, Input } from 'native-base';
 import { genericWriteAction } from '../../actions/GenericAction';
 import {styles} from './NumberButtonSelector.style'
 import { ScrollView } from 'react-native-gesture-handler';
 import { View } from 'react-native';
+import Section from '../Section';
+
 
 
 
@@ -29,111 +31,37 @@ const NumberButtonSelector = (props) => {
         submitFunction(val);
     }
 
-    const ModalContent = () => {
-        if (data.helperImg != null ){
-            var img = new ImageSelector()
-            const src = img.pathHandler(data.helperImg)
+    const renderSingleButton = (num) => {
             return (
-                <View style={styles.imgContainer}>
-                    <Layout style={styles.modalContent}>
-                        <Text>{data.tooltip}</Text>
-                        <Image source={src} style={styles.img}/>
-                    </Layout>
-                </View>
+                <Button onPress={() => submitField(num)} size={10}>{num}</Button>
             )
-        }else{
+        }
+
+        const renderButtons = () => {
+            let buttons = [];
+            for (let i = startRange; i <= endRange; i++) {
+                console.log(i);
+                let button;
+                if (i == 0) {
+                    button = renderSingleButton("None");
+                } else {
+                    button = renderSingleButton(i);
+                }
+                buttons.push(button);
+            }
             return(
-                <Layout style={styles.modalContent}>
-                    <Text>{data.tooltip}</Text>
-                </Layout>
+                <HStack space={4}>
+                {buttons}
+                <Input placeholder = "other" onChangeText = {submitField} size="md"/>
+                </HStack>
             )
         }
-    };
 
-        const HelperTooltip = () => {
-        if (data.helperText != null && (data.tooltip != null||data.helperImg!=null)){
-            return (
-                <Layout style={styles.container}>
-                    <View style={styles.rowContainer}>
-                        <Text style={styles.helperText}>{data.helperText}</Text>
-                        <Button appearance='ghost' status='primary' icon={InfoIcon} onPress={toggleModal}>
-                            Info
-                        </Button>
-                        <Modal backdropStyle={styles.backdrop} visible={visible}>
-                            <Card style={styles.content} disabled={true}>
-                            {ModalContent()}
-                            <Button appearance='ghost' icon={CloseIcon} onPress={() => setVisible(false)}>
-                                Close
-                            </Button>
-                            </Card>
-                        </Modal>
-                    </View>
-                </Layout>
-            )
-        }
-        else if (data.helperText != null) {
-            return (<Text style={styles.helperText}>{data.helperText}</Text>)
-        }
-        else if (data.tooltip != null || data.helperImg != null) {
-            return (
-                <View style={styles.endRowcontainer}>
-                    <Button  appearance='ghost' status='primary' icon={InfoIcon} onPress={toggleModal}>
-                        Info
-                    </Button>
-                    <Modal backdropStyle={styles.backdrop} visible={visible}>
-                        <Card style={styles.content} disabled={true}>
-                        {ModalContent()}
-                        <Button appearance='ghost' icon={CloseIcon} onPress={() => setVisible(false)}>
-                            Dismiss
-                        </Button>
-                        </Card>
-                    </Modal>
-                </View>
-            )
-        } else {
-            return null;
-        }
-    }
-
-    const InfoIcon = (props) => (
-        <Icon {...props} name='info'/>
-    );
-    const CloseIcon = (props) => (
-        <Icon {...props} name='close-outline'/>
-    );
-
-    const toggleModal = () => {
-        setVisible(!visible);
-    };
-
-    const Header = () => (
-        <CardHeader title={title}/>
-    );
-
-    return (
-        <Card header={Header}>
-            <View style={{flex: 1, flexDirection:"row"}}>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={true} contentContainerStyle={styles.container}>
-                    <View style={styles.buttonSection}>
-                        <Button style={styles.buttonStyle} appearance={(selection == '0') ? 'filled':'outline'} onPress={() => submitField('0')}>None</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '1') ? 'filled':'outline'} onPress={() => submitField('1')}>1</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '2') ? 'filled':'outline'} onPress={() => submitField('2')}>2</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '3') ? 'filled':'outline'} onPress={() => submitField('3')}>3</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '4') ? 'filled':'outline'} onPress={() => submitField('4')}>4</Button>
-                        <Button style={styles.buttonStyle} appearance={(selection == '5') ? 'filled':'outline'} onPress={() => submitField('5')}>5</Button>
-                    </View>
-                    <View style={styles.questionInput}>
-                        <Input
-                            placeholder = "other"
-                            onChangeText = {submitField}
-                            value = {(selection > 5) ? selection:''}
-                            status={(selection > 5) ? 'primary':'basic'}
-                        />
-                    </View>
-                </ScrollView>
-            </View>
-        </Card>
-    )
+        return (
+            <Section title={title}>
+                {renderButtons()}
+            </Section>
+        )
 }
 
 const mapDispatchToProps = {
