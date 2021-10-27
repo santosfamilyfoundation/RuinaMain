371686cd235e578f017eb0df23afd2bd68289a11
Kristin Aoki <kaoki@vgsi.com>
2021-10-27 08:59:33 -0400

    Fix theme error

--- App.js
+++ App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { IconRegistry } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { mapping, light as theme } from '@eva-design/eva';
 import { AppNavigator } from './appNavigator';
 import configureStore from './store';
-import { NativeBaseProvider, Center } from 'native-base';
+import { NativeBaseProvider, Center, extendTheme } from 'native-base';
 const store = configureStore()
 
-const App = () => (
-  const theme = extendTheme({
-    components: {
-        Box: {
-            .section: {
-                baseStyle: {
-                    rounded: 'md',
-                    bg: 'light.300'
-                }
-            }
-        }
-    }
-  })
-
-  <React.Fragment>
-    <IconRegistry icons={EvaIconsPack}/>
-    <NativeBaseProvider theme={theme}>
-      <Provider store = { store }>
-        <AppNavigator/>
-      </Provider>
-    </NativeBaseProvider>
-  </React.Fragment>
-);
+const App = () => {
+//  const theme = extendTheme({
+//    components: {
+//        Box: {
+//            .section: {
+//                baseStyle: {
+//                    rounded: 'md',
+//                    bg: 'light.300'
+//                }
+//            }
+//        }
+//    }
+//  })
+  return (
+      <React.Fragment>
+        <NativeBaseProvider>
+          <Provider store = { store }>
+            <AppNavigator/>
+          </Provider>
+        </NativeBaseProvider>
+      </React.Fragment>
+  )
+};
 
 export default App;
