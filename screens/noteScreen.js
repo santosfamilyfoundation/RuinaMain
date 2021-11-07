82a20ac91665f68c4b2b112c23053a77c79b70d5
18chowdhary <schowdhary@olin.edu>
2021-11-07 14:31:34 -0500

    Fix note screen UI kitten errors

--- screens/noteScreen.js
+++ screens/noteScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Icon, TopNavigationAction } from '@ui-kitten/components';
+//import { Icon, TopNavigationAction } from '@ui-kitten/components';
 import Note from '../components/modules/Note'
 
 export const NoteScreen = ({ navigation }) => {
-  return (
-          <Note
-            navigation = { navigation }
-            BackAction = { () => <TopNavigationAction icon={ (style) => <Icon {...style} name='arrow-back' /> }
-                                                  onPress={() => navigation.goBack()}/> }
-          />
+  return ( "Hello, World!"
+//          <Note
+//            navigation = { navigation }
+//            BackAction = { () => <TopNavigationAction icon={ (style) => <Icon {...style} name='arrow-back' /> }
+//                                                  onPress={() => navigation.goBack()}/> }
+//          />
   );
 };
