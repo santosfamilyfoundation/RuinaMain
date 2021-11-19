7ff91858fd487267381a8165dd317cc4c41f4c15
18chowdhary <schowdhary@olin.edu>
2021-11-19 09:49:06 -0500

    try to fix dependencies

--- utils/dependencyHelper.js
+++ utils/dependencyHelper.js
@@ -1,5 +1,5 @@
-export function dependencyParser(response, data, dependencyID) {
-    console.log('dependencyId:', dependencyID);
+export function dependencyParser(response, data, formID) {
+    console.log('dependencyId:', formID);
     var renderComponent = false;
     if (data.questionDependency == undefined) {
         renderComponent = true;
@@ -10,20 +10,20 @@ export function dependencyParser(response, data, dependencyID) {
             let tarOptionCode = tarQuesArr[i].dependencyOptionCode
             var findResponse;
             for (let j = response.length-1; j >= 0; j--) {
-                if (dependencyID != undefined && response[j].vehicleID != undefined) {
-                    let tarVehicle = dependencyID[1]
-                    switch (dependencyID.length) {
+                if (formID != undefined && response[j].vehicleID != undefined) {
+                    let tarVehicle = formID[1]
+                    switch (formID.length) {
                         case 2:
                             if (response[j].question == tarName && response[j].vehicleID == tarVehicle){
                                 findResponse = response[j]}
                             break;
                         case 3:
-                            let tarPassenger = dependencyID[2] 
+                            let tarPassenger = formID[2] 
                             if (response[j].question == tarName && response[j].vehicleID == tarVehicle && response[j].passengerID == tarPassenger){
                                 findResponse = response[j]}
                             break;
                         case 4:
-                            let tarNonmotorist = dependencyID[3]
+                            let tarNonmotorist = formID[3]
                             if (response[j].question == tarName && response[j].nonmotoristID == tarNonmotorist){
                                 findResponse = response[j]}
                             break;
