40277fd259c3e17ee207d7059dda0a760c7575c1
Kristin Aoki <kaoki@vgsi.com>
2022-02-08 23:12:52 -0500

    Remove unused packages and code

--- android/app/src/main/java/com/ruina/MainApplication.java
+++ android/app/src/main/java/com/ruina/MainApplication.java
@@ -14,13 +14,9 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.anyline.reactnative.AnylinePackage;
-import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
-import io.invertase.firebase.RNFirebasePackage;
-
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -35,7 +31,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          packages.add(new RNFirebaseStoragePackage());
           return packages;
         }
 
@@ -57,7 +52,6 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager()); // Remove this line if you don't want Flipper enabled
-    //FirebaseApp.initializeApp(this);
   }
 
     /**
