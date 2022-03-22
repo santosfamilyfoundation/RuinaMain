c814dd8fdf8a7c1edf089eafffb134ca490886af
CaseyJMay <42981072+CaseyJMay@users.noreply.github.com>
2022-03-22 13:12:15 -0400

    Update TextFieldValidation.js

--- src/utils/TextFieldValidation.js
+++ src/utils/TextFieldValidation.js
@@ -17,24 +17,18 @@ export class TextFieldValidation {
     validateInput(text){
           console.log('validateInput')
           if(text.length === 0){
-            console.log('false')
             this.status = false;
           }
           else if(text.length <= 500) {
-              console.log('true')
               this.status = true;
           }
           else{
-            console.log('false')
             this.status = false;
           }
-     console.log(this.status)
 
     }
     submitField(text){
-        console.log('submitField')
         this.validateInput(text)
-        console.log("this.status: ", this.status)
 
     }
 }
