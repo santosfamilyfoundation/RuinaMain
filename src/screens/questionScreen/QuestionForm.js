4cc8e019bbaaeb3e282f1f1576768a892e3d9135
Casey May <cmay@olin.edu>
2022-03-31 16:00:13 -0400

    Adding questionForm.js fix

--- src/screens/questionScreen/QuestionForm.js
+++ src/screens/questionScreen/QuestionForm.js
@@ -1,260 +1,303 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { SafeAreaView } from "react-navigation";
-import {
-	Divider,
-	Box,
-	Accordion,
-	VStack,
-	Button,
-	Center,
-	Spinner,
-} from "native-base";
-import { styles } from "../../autocomponentContainer/AutoComponentContainer.style";
-import { ScrollView } from "react-native-gesture-handler";
-import MultiButtonSelector from "../../components/buttonSelectors/MultiButtonSelector";
-import NumberButtonSelector from "../../components/buttonSelectors/NumberButtonSelector";
-import QuestionAutoCompleteDropDown from "../../components/dropdowns/QuestionAutoCompleteDropDown";
-import DropDownSingleSelect from "../../components/dropdowns/DropDownSingleSelect";
-import OpenTextField from "../../components/textFields/OpenTextField";
-import OpenTextFieldWithSelection from "../../components/textFields/OpenTextFieldWithSelection";
-import AdvancedOpenTextField from "../../components/textFields/AdvancedOpenTextField";
-import AdvancedDropDown from "../../components/dropdowns/AdvancedDropDown";
-import DropDownMultiSelect from "../../components/dropdowns/DropDownMultiSelect";
-import CountyDropDown from "../../components/dropdowns/CountyDropDown";
-import LargeTextField from "../../components/textFields/LargeTextField";
-import QuestionHeader from "../../components/QuestionHeader";
-import { updateDriver } from "../../actions/DriverAction";
-import { updateNonmotorist } from "../../actions/NonmotoristAction";
-import { updateVehicle } from "../../actions/VehicleAction";
-import { updatePassenger } from "../../actions/PassengerAction";
-import { updateRoad } from "../../actions/RoadAction";
-import { updateResponse } from "../../actions/StoryActions";
-import TopNavigation from "../../components/TopNavigation";
+import React from 'react';
+import { connect } from 'react-redux';
+import { SafeAreaView } from 'react-navigation';
+import { Divider, Box, Accordion, VStack, Button, Text } from 'native-base';
+import { styles } from '../../autocomponentContainer/AutoComponentContainer.style';
+import { ScrollView } from 'react-native-gesture-handler';
+import MultiButtonSelector from '../../components/buttonSelectors/MultiButtonSelector';
+import NumberButtonSelector from '../../components/buttonSelectors/NumberButtonSelector';
+import QuestionAutoCompleteDropDown from '../../components/dropdowns/QuestionAutoCompleteDropDown';
+import DropDownSingleSelect from '../../components/dropdowns/DropDownSingleSelect';
+import OpenTextField from '../../components/textFields/OpenTextField';
+import OpenTextFieldWithSelection from '../../components/textFields/OpenTextFieldWithSelection';
+import AdvancedOpenTextField from '../../components/textFields/AdvancedOpenTextField';
+import AdvancedDropDown from '../../components/dropdowns/AdvancedDropDown';
+import DropDownMultiSelect from '../../components/dropdowns/DropDownMultiSelect';
+import CountyDropDown from '../../components/dropdowns/CountyDropDown';
+import LargeTextField from '../../components/textFields/LargeTextField';
+import QuestionHeader from '../../components/QuestionHeader';
+import { updateDriver } from '../../actions/DriverAction';
+import { updateNonmotorist } from '../../actions/NonmotoristAction';
+import { updateVehicle } from '../../actions/VehicleAction';
+import { updatePassenger } from '../../actions/PassengerAction';
+import { updateRoad } from '../../actions/RoadAction';
+import { updateResponse } from '../../actions/StoryActions';
+import TopNavigation from '../../components/TopNavigation';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-class QuestionForm extends Component {
-	constructor(props) {
-		super(props);
+import {questions} from '../../data/questions';
 
-		this.state = {
-			questionDetail: props.questionDetail,
-			navigation: props.navigation,
-			updateResponse: props.updateResponse,
-		};
+const QuestionForm = (props) => {
+  const {
+    questionDetail,
+    updateDriver,
+    updateNonmotorist,
+    updatePassenger,
+    updateVehicle,
+    updateRoad,
+    BackAction,
+    navigation,
+    updateResponse,
+  } = props
 
-		this.sections = undefined;
-		this.question = this.props.questionDetail.questions;
-    console.log('questions passed to this component:', this.question);
-		this.loading = true;
-	}
+  // filter question information based on section type
+  const filterQuestionsData = (questionType) => {
+    return questions.data.filter(question => question.display.includes(questionType));
+  }
+  const navigateToAdvanced = (place, props) =>{
+      navigation.navigate(place, props);
+    }
 
-	navigateToAdvanced(place, props) {
-		this.state.navigation.navigate(place, props);
-	}
+   const questionProps = (type, res) => {
+      const PublicObj = {
+        data: res.data,
+        key: res.data.id,
+        id:res.detail.objectID,
+        dependencyID:res.detail.dependencyID,
+        reducer:res.setting.reducer,
+        submitFunction:res.setting.submitFunction,
+        updateResponse:updateResponse
+      }
+      const obj =
+      {
+        dropdown: {
+          ...PublicObj
+        },
+        dropdownMultiSelect: {
+          ...PublicObj
+        },
+        openTextBox: {
+          ...PublicObj
+        },
+        openTextBoxWithSelection: {
+          ...PublicObj
+        },
+        advancedOpenTextBox: {
+           ...PublicObj,
+          pageChange: navigateToAdvanced,
+          importFrom: res.data.autoMethod
+        },
+        advancedDropDown: {
+          ...PublicObj,
+          pageChange: navigateToAdvanced,
+          importFrom: res.data.autoMethod
+        },
+        countyDropDown: {
+          ...PublicObj
+        },
+        largeTextField: {
+           ...PublicObj
+        },
+        multiButton: {
+          ...PublicObj
+        },
+        numberButton: {
+          ...PublicObj
+        },
+        autoCompleteDropdown: {
+          ...PublicObj
+        },
+        questionHeader: {
+          data: res.data,
+          dependencyID:res.detail.dependencyID,
+        },
+      }
+      return  obj[type]
+    }
+  const questionData = (type) => {
+    const obj = {
+      Driver: {
+        reducer: "driverReducer",
+        submitFunction: updateDriver,
+        actionType: "UPDATEDRIVER",
+        questionsData : filterQuestionsData('driver'),
+      },
+      Nonmotorist: {
+        reducer: "nonmotoristReducer",
+        submitFunction: updateNonmotorist,
+        actionType: "UPDATENONMOTORIST",
+        questionsData : filterQuestionsData('nonmotorist'),
+      },
+      Passenger: {
+        reducer: "passengerReducer",
+        submitFunction: updatePassenger,
+        actionType: "UPDATEPASSENGER",
+        questionsData : filterQuestionsData('passenger'),
+      },
+      Vehicle: {
+        reducer: "vehicleReducer",
+        submitFunction: updateVehicle,
+        actionType: "UPDATEVEHICLE",
+        questionsData : filterQuestionsData('vehicle'),
+      },
+      Road: {
+        reducer: "roadReducer",
+        submitFunction: updateRoad,
+        actionType: "UPDATEROAD",
+        questionsData : filterQuestionsData('road'),
+      }
+    }
+    return type?obj[type]:obj
+  }
+   const renderSingleQuestion = (type, props) => {
+      switch (type) {
+        case 'dropdown':
+          return (
+            <DropDownSingleSelect
+              {...props}
+            />
+          )
 
-	questionProps(type, res) {
-		const PublicObj = {
-			data: res.data,
-			key: res.data.id,
-			id: res.detail.objectID,
-			dependencyID: res.detail.dependencyID,
-			reducer: res.setting.reducer,
-			submitFunction: res.setting.submitFunction,
-			updateResponse: this.state.updateResponse,
-		};
-		const obj = {
-			dropdown: {
-				...PublicObj,
-			},
-			dropdownMultiSelect: {
-				...PublicObj,
-			},
-			openTextBox: {
-				...PublicObj,
-			},
-			openTextBoxWithSelection: {
-				...PublicObj,
-			},
-			advancedOpenTextBox: {
-				...PublicObj,
-				pageChange: this.navigateToAdvanced,
-				importFrom: res.data.autoMethod,
-			},
-			advancedDropDown: {
-				...PublicObj,
-				pageChange: this.navigateToAdvanced,
-				importFrom: res.data.autoMethod,
-			},
-			countyDropDown: {
-				...PublicObj,
-			},
-			largeTextField: {
-				...PublicObj,
-			},
-			multiButton: {
-				...PublicObj,
-			},
-			numberButton: {
-				...PublicObj,
-			},
-			autoCompleteDropdown: {
-				...PublicObj,
-			},
-			questionHeader: {
-				data: res.data,
-				dependencyID: res.detail.dependencyID,
-			},
-		};
-		return obj[type];
-	}
+        case 'dropdownMultiSelect':
 
-	renderSingleQuestion(type, props) {
-		switch (type) {
-			case "dropdown":
-				return <DropDownSingleSelect {...props} />;
+          return (
+            <DropDownMultiSelect
+              {...props}
+            />
+          )
 
-			case "dropdownMultiSelect":
-				return <DropDownMultiSelect {...props} />;
+        case 'openTextBox':
 
-			case "openTextBox":
-				const comp = <OpenTextField {...props} />;
-				return comp;
+          return (
+            <OpenTextField
+              {...props}
+            />
+          )
 
-			case "openTextBoxWithSelection":
-				return <OpenTextFieldWithSelection {...props} />;
+        case 'openTextBoxWithSelection':
 
-			case "advancedOpenTextBox":
-				return <AdvancedOpenTextField {...props} />;
+          return (
+            <OpenTextFieldWithSelection
+              {...props}
+            />
+          )
 
-			case "advancedDropDown":
-				return <AdvancedDropDown {...props} />;
+          case 'advancedOpenTextBox':
 
-			case "countyDropDown":
-				return <CountyDropDown {...props} />;
+            return (
+              <AdvancedOpenTextField
+              {...props}
+              />
+            )
 
-			case "largeTextField":
-				return <LargeTextField {...props} />;
+        case 'advancedDropDown':
 
-			case "multiButton":
-				return <MultiButtonSelector {...props} />;
-			case "numberButton":
-				return <NumberButtonSelector {...props} />;
-			case "autoCompleteDropdown":
-				return <QuestionAutoCompleteDropDown {...props} />;
-			case "questionHeader":
-				return <QuestionHeader {...props} />;
-			default:
-				return null;
-		}
-	}
+          return (
+            <AdvancedDropDown
+              {...props}
+            />
+          )
 
-	renderQuestions(questions) {
-		console.log("Beginning to render questions for section");
-		const questionList = questions.map((q) =>
-			this.renderSingleQuestion(
-				q.answerType,
-				this.questionProps(q.answerType, {
-					data: q,
-					setting: this.question,
-					detail: this.state.questionDetail,
-				})
-			)
-		);
-		return <VStack>{questionList}</VStack>;
-	}
+        case 'countyDropDown':
+          return (
+            <CountyDropDown
+              {...props}
+            />
+          )
 
-	renderSingleSection(item) {
-		console.log("Beginning to render a single section");
-		return (
-			<Accordion.Item>
-				<Accordion.Summary>
-					{item.sectionTitle}
-					<Accordion.Icon />
-				</Accordion.Summary>
-				<Accordion.Details>
-					{this.renderQuestions(item.questions)}
-				</Accordion.Details>
-			</Accordion.Item>
-		);
-	}
+        case 'largeTextField':
+          return (
+            <LargeTextField
+              {...props}
+            />
+          )
 
-	renderSections() {
-		console.log("Starting to render all sections");
-		const result = (
-			<Accordion index={[0]} mx={4} my={8}>
-				{this.question.questionsData.map((item) =>
-					this.renderSingleSection(item)
-				)}
-			</Accordion>
-		);
-		console.log("Finished rendering all sections");
-		this.sections = result;
-		this.loading = false;
-	}
+        case 'multiButton':
+          return (
+            <MultiButtonSelector
+              {...props}
+            />
+          )
+        case 'numberButton':
+          return (
+            <NumberButtonSelector
+                {...props}
+            />
+          )
+        case 'autoCompleteDropdown':
+          return (
+            <QuestionAutoCompleteDropDown
+              {...props}
+            />
+          )
+        case 'questionHeader':
+            return(
+                <QuestionHeader
+                    {...props}
+                />
+            )
+      }
+    }
 
-  componentDidMount() {
-    console.log("component did update");
-	this.renderSections();
-    this.forceUpdate(); 
+    const question = questionData(questionDetail.type)
+    const renderedQuestions = (questions) => {
+        let questionList = []
+        for (const q of questions) {
+            const obj = {data:q, setting: question, detail:questionDetail}
+            // console.log('questionDetail.type:', questionDetail.type);
+            const dom = renderSingleQuestion(q.answerType, questionProps(q.answerType, obj));
+            questionList.push(dom)
+        }
+        return (
+            <VStack>
+                {questionList}
+            </VStack>
+        )
+    };
+  const renderSingleSection = (item) => {
+    return (
+        <Accordion.Item>
+            <Accordion.Summary>
+                {item.sectionTitle}
+                <Accordion.Icon />
+            </Accordion.Summary>
+            <Accordion.Details>
+                {renderedQuestions(item.questions)}
+            </Accordion.Details>
+        </Accordion.Item>
+    )
   }
 
-	render() {
-		console.log("rendering");
-		if (this.loading) {
-			console.log("rendering loading");
-			return (
-				<SafeAreaView style={styles.container}>
-					<TopNavigation
-						title={`Questions on ${this.state.questionDetail.name}`}
-						backButton
-						navigation={this.state.navigation}
-					/>
-					<Center flex={1}>
-						<Spinner accessibilityLabel="loadingScreen" size="lg" />
-					</Center>
-				</SafeAreaView>
-			);
-		} else {
-			console.log("rendering sections");
-			return (
-				<SafeAreaView style={styles.container}>
-					<TopNavigation
-						title={`Questions on ${this.state.questionDetail.name}`}
-						backButton
-						navigation={this.state.navigation}
-					/>
-					<VStack>
-						<ScrollView>
-							<Box>{this.sections}</Box>
-						</ScrollView>
-						<Divider />
-						<Box p={4}>
-							<Button
-								onPress={() => {
-									this.state.navigation.goBack();
-								}}
-							>
-								Save Progress
-							</Button>
-						</Box>
-					</VStack>
-				</SafeAreaView>
-			);
-		}
-	}
+
+  const renderSections = (question) => {
+    return (
+        <Accordion index={[0]} mx={4} my={8}>
+            { question.questionsData.map(item => renderSingleSection(item)) }
+        </Accordion>
+    )
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <TopNavigation title={`Questions on ${questionDetail.name}`} backButton navigation={navigation}/>
+      <ScrollView>
+        <Box>
+            {renderSections(question)}
+        </Box>
+      </ScrollView>
+      <Divider />
+      <Box p={4}>
+        <Button onPress={()=> {navigation.goBack()}}>Save Progress</Button>
+      </Box>
+        <VStack alignItems="center" mt={2} mb={8}>
+          <TouchableOpacity onPress={() => Linking.openURL('https://forms.gle/aXVjxVrQU6jm3KUx6')}><Text style={{ color: 'blue' }}>Submit Feedback</Text></TouchableOpacity>
+        </VStack>
+    </SafeAreaView>
+  );
 }
 
 const mapDispatchToProps = {
-	updateDriver,
-	updateNonmotorist,
-	updateVehicle,
-	updatePassenger,
-	updateRoad,
-	updateResponse,
-};
+  updateDriver,
+  updateNonmotorist,
+  updateVehicle,
+  updatePassenger,
+  updateRoad,
+  updateResponse,
+}
 
 const mapStateToProps = (state) => {
-	return {};
+
+  return {
+      };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(QuestionForm);
