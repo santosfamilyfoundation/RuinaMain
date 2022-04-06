b95d860782a285e18da70dc5b669e777cf5353c5
18chowdhary <schowdhary@olin.edu>
2022-04-06 13:39:06 -0400

    Document TopNavigation component

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,27 +1,49 @@
-import React from 'react';
-import { HStack, Divider, Heading, IconButton, ArrowBackIcon, Box } from 'native-base';
+```
+    The TopNavigation is a functional component that provides navigation to go between screens.
+    The TopNavigation bar stays at the top of every screen. At the moment, the TopNavigation bar 
+    only allows navigation back one screen (if the back button is visible).
+
+    Props:
+        title - the text that should display inside the top navigation bar (generally the name of the screen or "Ruina").
+        backButton - a boolean that controls whether the back button should be visible.
+        navigation - Navigation object to enable switching between screens
+```;
+
+import React from "react";
+import {
+   HStack,
+   Divider,
+   Heading,
+   IconButton,
+   ArrowBackIcon,
+} from "native-base";
 
 const TopNavigation = (props) => {
-    const { title, backButton, navigation } = props
+   const { title, backButton, navigation } = props;
 
-    return(
-        <>
-         <HStack m={4} alignItems='center' justifyContent={backButton||props.children ? 'space-between': 'center'}>
-            {backButton ?
-                <IconButton
-                 icon={<ArrowBackIcon name="arrow-back"/>}
-                 onPress={()=>{navigation.goBack()}}
-                />
-                : null
+   return (
+      <>
+         <HStack
+            m={4}
+            alignItems="center"
+            justifyContent={
+               backButton || props.children ? "space-between" : "center"
             }
+         >
+            {backButton ? (
+               <IconButton
+                  icon={<ArrowBackIcon name="arrow-back" />}
+                  onPress={() => {
+                     navigation.goBack();
+                  }}
+               />
+            ) : null}
             <Heading textAlign="center">{title}</Heading>
-            <HStack>
-                {props.children}
-            </HStack>
+            <HStack>{props.children}</HStack>
          </HStack>
-         <Divider/>
-        </>
-    )
-}
+         <Divider />
+      </>
+   );
+};
 
-export default TopNavigation
+export default TopNavigation;
