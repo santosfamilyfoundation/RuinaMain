12c2ec52c747eb8d2acf65d6364d893e4ba0e78f
18chowdhary <schowdhary@olin.edu>
2022-04-06 11:38:31 -0400

    Reduce tab width

--- src/data/parseStateCounty.js
+++ src/data/parseStateCounty.js
@@ -7,7 +7,7 @@ been run once, it should not need to be run again unless stateCountyMapping.json
 To run: 
 cd <project root directory>
 node data/parseStateCounty.js
-```;
+```
 const usStateAbbrev = require("us-state-codes"); // Maryland -> MD
 const fips = require("fips-county-codes");
 const fs = require("fs");
@@ -17,58 +17,58 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     This function makes an HTTP request to retrieve data from the us-atlas library via URL.
 */
 function Get(url) {
-     var Httpreq = new XMLHttpRequest(); // a new request
-     Httpreq.open("GET", url, false);
-     Httpreq.send(null);
-     return Httpreq.responseText;
+   var Httpreq = new XMLHttpRequest(); // a new request
+   Httpreq.open("GET", url, false);
+   Httpreq.send(null);
+   return Httpreq.responseText;
 }
 // US counties
 const us = JSON.parse(
-     Get("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json")
+   Get("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json")
 );
 
 let countiesByStates = {};
 // set up a mapping of state IDs and state names
 const statesByFips = new Map(
-     us.objects.states.geometries.map((state) => {
-          let stateName = state.properties.name;
-          countiesByStates[stateName] = [];
-          return [state.id, stateName];
-     })
+   us.objects.states.geometries.map((state) => {
+      let stateName = state.properties.name;
+      countiesByStates[stateName] = [];
+      return [state.id, stateName];
+   })
 );
 
 // set up a mapping of county IDs (FIPS code) and county names
 const countiesByFips = new Map(
-     us.objects.counties.geometries.map((county) => {
-          let fip = county.id; // county fip code
+   us.objects.counties.geometries.map((county) => {
+      let fip = county.id; // county fip code
 
-          // Get the state code from fip and look up the corresponding state name
-          // first 2 digit of fip is state, get full state name
-          let countyStateName = statesByFips.get(fip.slice(0, 2)); 
+      // Get the state code from fip and look up the corresponding state name
+      // first 2 digit of fip is state, get full state name
+      let countyStateName = statesByFips.get(fip.slice(0, 2));
 
-          // Get abbreviated state name (fips-county-codes API uses abbrev state name)
-          // get state abbrev ex MD
-          let stateCode = usStateAbbrev.getStateCodeByStateName(countyStateName); 
+      // Get abbreviated state name (fips-county-codes API uses abbrev state name)
+      // get state abbrev ex MD
+      let stateCode = usStateAbbrev.getStateCodeByStateName(countyStateName);
 
-          // Try getting full county name from fips-county-codes library if failed, use default name in json
-          let countyName = county.properties.name;
-          try {
-               countyName = fips.get({
-                    fips: fip.slice(2, 5),
-                    state: stateCode,
-               }).county;
-          } catch (error) {}
+      // Try getting full county name from fips-county-codes library if failed, use default name in json
+      let countyName = county.properties.name;
+      try {
+         countyName = fips.get({
+            fips: fip.slice(2, 5),
+            state: stateCode,
+         }).county;
+      } catch (error) {}
 
-          // add county to corresponding list by state
-          countiesByStates[countyStateName].push(countyName);
-          return [fip, countyName];
-     })
+      // add county to corresponding list by state
+      countiesByStates[countyStateName].push(countyName);
+      return [fip, countyName];
+   })
 );
 
 // turn county map into a JSON string and output to file
 let json = JSON.stringify(countiesByStates);
 fs.writeFile("stateCountyMapping.json", json, function (err, data) {
-     fs.writeFile("errorLog.txt", data, function (err, result) {
-          if (err) console.log("error", err);
-     });
+   fs.writeFile("errorLog.txt", data, function (err, result) {
+      if (err) console.log("error", err);
+   });
 });
