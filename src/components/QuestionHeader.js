297c03616c51bdd701582783d7dc36ddfc95e28b
18chowdhary <schowdhary@olin.edu>
2022-04-06 11:37:30 -0400

    Reformat tabs for QuestionHeader

--- src/components/QuestionHeader.js
+++ src/components/QuestionHeader.js
@@ -10,33 +10,29 @@ import { dependencyParser } from "../utils/dependencyHelper";
 import { Heading, Text, VStack } from "native-base";
 
 const QuestionHeader = (props) => {
-     const { data, dependencyID } = props;
+   const { data, dependencyID } = props;
 
-     // check if this should be shown based on dependencies
-     var renderComponent = true;
-     if (data.questionDependency !== undefined && props.response !== null) {
-          renderComponent = dependencyParser(
-               props.response,
-               data,
-               dependencyID
-          );
-     }
-     
-     if (renderComponent) {
-          return (
-               <VStack ml={4} mt={4}>
-                    <Heading size="md">{data.question}</Heading>
-                    <Text>{data.helperText}</Text>
-               </VStack>
-          );
-     } else {
-          return null;
-     }
+   // check if this should be shown based on dependencies
+   var renderComponent = true;
+   if (data.questionDependency !== undefined && props.response !== null) {
+      renderComponent = dependencyParser(props.response, data, dependencyID);
+   }
+
+   if (renderComponent) {
+      return (
+         <VStack ml={4} mt={4}>
+            <Heading size="md">{data.question}</Heading>
+            <Text>{data.helperText}</Text>
+         </VStack>
+      );
+   } else {
+      return null;
+   }
 };
 
 const mapStateToProps = (state) => {
-     const { response } = state.storyReducer;
-     return { response };
+   const { response } = state.storyReducer;
+   return { response };
 };
 
 export default connect(mapStateToProps)(QuestionHeader);
