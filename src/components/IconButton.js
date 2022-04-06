c0c4c5a53f901f57ef11fa4c35225794243f8c25
18chowdhary <schowdhary@olin.edu>
2022-04-06 11:37:14 -0400

    Reformat tabs for IconButton

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -7,25 +7,20 @@ import React from "react";
 import { Button, Text, HStack } from "native-base";
 
 const IconButton = (props) => {
-     const { text, onPress, icon, leftMargin, rightMargin, topMargin } = props;
+   const { text, onPress, icon, leftMargin, rightMargin, topMargin } = props;
 
-     return (
-          <Button
-               onPress={onPress}
-               ml={leftMargin}
-               mr={rightMargin}
-               mt={topMargin}
-          >
-               <HStack alignItems="center">
-                    {icon}
-                    {text ? (
-                         <Text ml={2} color="white" fontWeight="semibold">
-                              {text}
-                         </Text>
-                    ) : null}
-               </HStack>
-          </Button>
-     );
+   return (
+      <Button onPress={onPress} ml={leftMargin} mr={rightMargin} mt={topMargin}>
+         <HStack alignItems="center">
+            {icon}
+            {text ? (
+               <Text ml={2} color="white" fontWeight="semibold">
+                  {text}
+               </Text>
+            ) : null}
+         </HStack>
+      </Button>
+   );
 };
 
 export default IconButton;
