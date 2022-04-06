2b3ae09c16152753563fb97cb73374867f11de5b
18chowdhary <schowdhary@olin.edu>
2022-04-06 11:44:34 -0400

    Update header comment for Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,3 +1,13 @@
+```
+    The Section is a functional component that encapsulates a section of questions.
+    It renders a faint border around a set of questions, a heading for the section,
+    a divider, and all of the questions for the section.
+
+    Props:
+        title - the text that should be rendered in the Heading of the Section.
+        children - should be the Questions and QuestionSections that should be rendered in this Section
+```
+
 import React, {Component} from 'react';
 import { Heading, Divider, Box, FormControl, VStack } from 'native-base';
 
