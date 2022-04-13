7f6191905e218a91dd16ae8c69ba42ab7fbd8a70
Kristin Aoki <kaoki@vgsi.com>
2022-04-13 10:58:31 -0400

    Add code documentation

--- src/screens/map/mapScreen.js
+++ src/screens/map/mapScreen.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import Map from './Map'
+/*
+  This file is used by the appNavigation component to render the Map screen.
+*/
+import React from "react";
+import Map from "./Map";
 
-export const MapScreen = ({ navigation}) => {
-
-  return (
-          <Map
-            navigation = { navigation }
-          />
-  );
+export const MapScreen = ({ navigation }) => {
+  return <Map navigation={navigation} />;
 };
